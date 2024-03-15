1c685d1148ad6c0d7220f41adb146f91b350ff36
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-03-15 15:43:09 -0400

    Add viewport, camera, and colors for rays
    Viewport and camera added to give 3D space. Changed values for better 3D
    camera maths. Along with adding in ray colors as well.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,48 @@
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <cstddef>
 #include <iostream>
 #include <type_traits>
 
+
 int main()
 {
-    int image_width = 256;
-    int image_height = 256;
+    // Note: An image's aspect ratio can be found by the ratio of its height and width (width/height)
+    auto aspect_ratio = 16.0/9.0;
+    int image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1
+    // Reason why we wantto ensure it's 1 for the following reason
+    // Make sure that we can scale up or down the image by changing its width
+    // Won't throw off our desired aspect ratio (that we set already)
+    int image_height = static_cast<int>(image_width/aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+    auto focal_length = 1.0;
+    // Viewport widths less than one are okay since they are real valued
+    // Viewports are important due to allowing us to setup our 3D world in a way that contains the grid of pixels
+    // This ensures that the objects that are bounded to the viewport share the same aspect ratio across the board
+    auto viewport_height = 2.0;
+    // Reason we don't use the aspect ratio var is because we need the viewport to match the image porportions
+    auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+    // Note: Distance between two adjacent pixels is called pixel spacing and the standard is square pixels
+    auto camera_center = point3(0,0,0);
+
+    // Calcuate the
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -17,7 +51,11 @@ int main()
         std::clog << "\rScanlines remaining: " << (image_height - y) << ' ' << std::flush;
         for (int x = 0; x < image_width; ++x) 
         {
-            auto pixel_color = color(double(x)/(image_width-1), double(y)/(image_height-1), 0); 
+            auto pixel_center = pixel00_loc + (x * pixel_delta_u) + (y * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
