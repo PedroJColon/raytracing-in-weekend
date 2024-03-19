88e9481f79bf910f00a1b9e510f36a5d3b10676c
pjc-dev <64868232+PedroJColon@users.noreply.github.com>
2024-03-19 01:54:57 -0400

    Add hittable lists functionality
    Hittable lists now has functionality for keeping track of multiple objs

--- hittable.h
+++ hittable.h
@@ -5,7 +5,7 @@
 #include "vec3.h"
 
 class hit_record {
-public:
+    public:
     point3 p;
     vec3 normal;
     double t;
@@ -23,9 +23,9 @@ public:
 
 class hittable {
     public:
-        virtual ~hittable() = default;
+    virtual ~hittable() = default;
 
-        virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
 };
 
 
