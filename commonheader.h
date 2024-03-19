541915348df774b3a25bd44d0dbd84f40612a6d8
pjc-dev <64868232+PedroJColon@users.noreply.github.com>
2024-03-19 02:15:06 -0400

    Add commonheader and main.cpp new functionality
    Commonheader made to hold common operations and main making use of new functionality made.

---
+++ commonheader.h
@@ -0,0 +1,31 @@
+#ifndef COMMONHEADER_H
+#define COMMONHEADER_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+// usings
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// utility functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+
+// common header files
+
+#include "ray.h"
+#include "vec3.h"
+
+#endif
