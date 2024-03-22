701a4e51042b84b21bb1add9da06a7b944525702
pjc-dev <64868232+PedroJColon@users.noreply.github.com>
2024-03-22 13:33:13 -0400

    Add new camera class header file
    Start refactoring code to include camera class for
    better cohesion.

---
+++ src/camera.h
@@ -0,0 +1,27 @@
+#ifdef CAMERA_H
+#define CAMERA_H
+
+#include "rtweekend.h"
+
+#include "color.h"
+#include "hittable.h"
+
+class camera {
+    public:
+
+    void render() {
+
+    }
+
+    private:
+
+    void init() {
+
+    }
+
+    color ray_color () {
+
+    }
+}
+
+#endif
