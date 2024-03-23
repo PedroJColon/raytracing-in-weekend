7e489d540f35d77f081c9fb1574825c1228d2ef9
pjc-dev <64868232+PedroJColon@users.noreply.github.com>
2024-03-23 01:41:45 -0400

    Add rand ulit functions
    Added random ulit functions to help with anti-aliasing.

--- src/commonheader.h
+++ src/commonheader.h
@@ -2,6 +2,7 @@
 #define COMMONHEADER_H
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -22,6 +23,15 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+inline double random_double() {
+    // TODO: Write down comment
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // TODO: Write down coment
+    return min + (max-min)*random_double();
+}
 
 // common header files
 
