35c8761206a5885652f1f3defa665bc59e96973b
pjc-dev <64868232+PedroJColon@users.noreply.github.com>
2024-03-29 12:10:08 -0400

    Fix edge cases to prevent stack from being blown
    Due to recursion being used and needing protection
    against potiental stack blow, maximum recursion
    depth was added.

--- src/camera.h
+++ src/camera.h
@@ -16,7 +16,8 @@ public:
   // width (width/height)
   double aspect_ratio = 1.0; // Ratio of image width over height
   int image_width = 100;     // Rendered image width in pixel count
-  int samples_per_pixel = 10;
+  int samples_per_pixel = 10; // Count of random samples for each pixel
+  int max_depth = 10; // Maximum number of ray bounces into scene
 
   void render(const hittable &world) {
     init();
@@ -30,7 +31,7 @@ public:
         color pixel_color(0, 0, 0);
         for (int sample = 0; sample < samples_per_pixel; ++sample) {
           ray r = get_ray(x, y);
-          pixel_color += ray_color(r, world);
+          pixel_color += ray_color(r, max_depth, world);
         }
         write_color(std::cout, pixel_color, samples_per_pixel);
       }
@@ -87,11 +88,16 @@ private:
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
   }
 
-  color ray_color(const ray &r, const hittable &world) const {
+  color ray_color(const ray &r, int depth, const hittable &world) const {
     hit_record rec;
+
+    // If we're exceeded ray bounce limit, no more light is gathered
+    if (depth <= 0)
+      return color(0, 0, 0);
+
     if (world.hit(r, interval(0, infinity), rec)) {
       vec3 direction = random_on_hemisphere(rec.normal);
-      return 0.5 * ray_color(ray(rec.p, direction), world);
+      return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
