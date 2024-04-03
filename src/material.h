7c787c1827868c812f561a7b4aee6196f3927221
pjc-dev <64868232+PedroJColon@users.noreply.github.com>
2024-04-03 12:08:06 -0400

    Add schilix approxmination and Hollow Glass Sphere

--- src/material.h
+++ src/material.h
@@ -2,9 +2,6 @@
 #define MATERIAL_H_
 
 #include "commonheader.h"
-#include "vec3.h"
-#include <cmath>
-#include <filesystem>
 
 class hit_record;
 
@@ -70,22 +67,25 @@ public:
     bool cannot_refract = refraction_ratio * sin_theta > 1.0;
     vec3 direction;
 
-    if (cannot_refract) {
+    if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double()) {
       direction = reflect(unit_direction, rec.normal);
     }
     else {
       direction = refract(unit_direction, rec.normal, refraction_ratio);
     }
-    //vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
-
-    
-    // scattered = ray(rec.p, refracted);
     scattered = ray(rec.p, direction);
     return true;
   }
 
 private:
   double ir; // Index of refraction
+
+  static double reflectance(double cosine, double ref_idx) {
+    //
+    auto r0 = (1-ref_idx) / (1+ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1-r0)*pow((1 - cosine),5);
+  }
 };
 
 #endif // MATERIAL_H_
