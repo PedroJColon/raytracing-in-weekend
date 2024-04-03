aa27f56923d1dac19f93bdb71f29a7ae70946678
pjc-dev <64868232+PedroJColon@users.noreply.github.com>
2024-04-03 12:14:23 -0400

    Add comment for function

--- src/material.h
+++ src/material.h
@@ -81,7 +81,7 @@ private:
   double ir; // Index of refraction
 
   static double reflectance(double cosine, double ref_idx) {
-    //
+    // Use Schlick's approximation for reflectance
     auto r0 = (1-ref_idx) / (1+ref_idx);
     r0 = r0 * r0;
     return r0 + (1-r0)*pow((1 - cosine),5);
