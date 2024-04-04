4cba9c5afb5ba98ed1d41b5baa3d511a039c94ba
pjc-dev <64868232+PedroJColon@users.noreply.github.com>
2024-04-03 20:39:16 -0400

    Add defocus blur to program

--- src/camera.h
+++ src/camera.h
@@ -6,7 +6,9 @@
 #include "color.h"
 #include "hittable.h"
 #include "material.h"
+#include "vec3.h"
 
+#include <cmath>
 #include <iostream>
 
 class camera {
@@ -18,9 +20,12 @@ public:
   int samples_per_pixel = 10; // Count of random samples for each pixel
   int max_depth = 10;         // MAxium number of ray bounces into scene
   double vfov = 90; // Vertical view angle (field of view)
-  point3 lookfrom = point3(0,0,-1);
-  point3 lookat = point3(0,0,0);
-  vec3 vup = vec3(0,1,0);
+  point3 lookfrom = point3(0,0,-1); // Where camera is looking from
+  point3 lookat = point3(0,0,0); // Point camera is looking at 
+  vec3 vup = vec3(0,1,0); // Camera relative up direction
+
+  double defocus_angle = 0; 
+  double focus_dist = 10;
 
   void render(const hittable &world) {
     init();
@@ -52,6 +57,8 @@ private:
   vec3 u; // Camera frame basis vectors
   vec3 v; // Camera frame basis vectors
   vec3 w; // Camera frame basis vectors
+  vec3 defocus_disk_u; // DEfocus disk horizontal radius
+  vec3 defocus_disk_v; // Defocus disk vertical radius
 
   void init() {
     // Calculate the image height, and ensure that it's at least 1
@@ -64,14 +71,14 @@ private:
     center = lookfrom;
 
     // Camera
-    auto focal_length = (lookfrom - lookat).length();
+    // auto focal_length = (lookfrom - lookat).length();
     auto theta = degrees_to_radians(vfov);
     auto h = tan(theta/2);
     // Viewport widths less than one are okay since they are real valued
     // Viewports are important due to allowing us to setup our 3D world in a way
     // that contains the grid of pixels This ensures that the objects that are
     // bounded to the viewport share the same aspect ratio across the board
-    auto viewport_height = 2.0 * h * focal_length;
+    auto viewport_height = 2.0 * h * focus_dist;
     // Reason we don't use the aspect ratio var is because we need the viewport
     // to match the image porportions
     auto viewport_width =
@@ -79,14 +86,15 @@ private:
     // Note: Distance between two adjacent pixels is called pixel spacing and
     // the standard is square pixels
 
+    // Calcuate the u,v,w unit basis vectors for the camera coordinate frame
     w = unit_vector(lookfrom - lookat);
     u = unit_vector(cross(vup, w));
     v = cross(w, u);
 
     // Helps us calcuate the vectors across horizontal axis and down the
     // vertical axis
-    auto viewport_u = viewport_width * u;
-    auto viewport_v = viewport_height * v;
+    auto viewport_u = viewport_width * u; // Vector across viewport horizontal edge
+    auto viewport_v = viewport_height * v; // Vector down viewport vertical edge
 
     // Helps us calcuate where to put the pixel values by using the delta
     // vectors as our guide This way, our viewport and pixel grid are evenly
@@ -95,10 +103,15 @@ private:
     pixel_delta_v = viewport_v / image_height;
 
     // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = center - (focal_length * w) - viewport_u/2 - viewport_v/2;
+    auto viewport_upper_left = center - (focus_dist * w) - viewport_u/2 - viewport_v/2;
     // auto viewport_upper_left =
     //     center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Calcuate camera defocus disk basis vectors
+    auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle/2));
+    defocus_disk_u = u * defocus_radius;
+    defocus_disk_v = v * defocus_radius;
   }
 
   color ray_color(const ray &r, int depth, const hittable &world) const {
@@ -131,14 +144,20 @@ private:
   }
 
   ray get_ray(int i, int j) const {
-    // Get randomy sampled camera ray for the pixel location of i and j
+    // Get randomy sampled camera ray for the pixel location of i and j, originating from camera defocus disk
     auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
     auto pixel_sample = pixel_center + pixel_sample_square();
 
-    auto ray_origin = center;
+    auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
     auto ray_direction = pixel_sample - ray_origin;
 
     return ray(ray_origin, ray_direction);
+  }
+
+  point3 defocus_disk_sample() const {
+    // Returns a random point in the camera defocus disk
+    auto p = random_in_unit_disk();
+    return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
   }
 };
 
